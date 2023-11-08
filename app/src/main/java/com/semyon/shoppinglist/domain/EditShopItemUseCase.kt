459db54da7cac1ae76395c77720bac562325d339
4kosmos5 <altushkin.semen@gmail.com>
2023-11-08 18:26:34 +0500

    data and domain

---
+++ app/src/main/java/com/semyon/shoppinglist/domain/EditShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.semyon.shoppinglist.domain
+
+class EditShopItemUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun editShopItem(shopItem: ShopItem) {
+        shopListRepository.editShopItem(shopItem)
+    }
+}
--- app/src/main/java/com/semyon/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/semyon/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.semyon.shoppinglist
+package com.semyon.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.semyon.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
